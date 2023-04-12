862029e37a8fd35fb5e116021b2d3cb2ff872999
Saidumarov <saidumarovjafarxon@gmail.com>
2023-04-12 15:50:48 +0500

    new commit

--- aap.js
+++ aap.js
@@ -1,17 +1,3 @@
-// let bodyColor =document.querySelector("body")
-
-// let h1 = document.querySelector(".h1")
-
-// function ok(){
-//     // h1.style.background = "red"
-//     // h1.style.color = "yellow"
-//     let input = prompt("rrrrrrr")
-// bodyColor.style.backgroundColor = input
-
-
-// }
-
-
 
 // 1 talik
 
@@ -19,66 +5,118 @@ let in1 = document.querySelector(".in1")
 let in2 = document.querySelector(".in2")
 let ttx = document.querySelector(".ttx")
 
-let count = -1
+let count = -1  
 
-function ok1(){
-   
+function ok1() {
     count++
-    ttx.textContent = 1 +count ;
-    if (count >8) {
-        ttx.style.backgroundColor = "red";
-        
-    } 
+    ttx.textContent = 1 + count;
+    if (count > 8) {
+        ttx.style.backgroundColor = String("#2742DF");
+        ttx.style.color = "white"
 
-    else if(count > -1){
-        ttx.style.backgroundColor = "white"; 
+    } else if (count > -2) {
+        ttx.style.backgroundColor = "white";
     }
-    
-   
-}
 
-function ok2(){
-    count--
-    ttx.textContent = 1+ count;
+    if (count > 8) {
+        ttx.style.color = "white"
+
+    } else if (count < 8) {
+        ttx.style.color = "black"
+    }
     if (count < -1) {
-        ttx.style.backgroundColor = "yellow"
-     
-    }    else if(count < 10){
-        ttx.style.backgroundColor = "white"; 
+        ttx.style.color = "white"
     }
+
+
 }
-    
+
+
+function ok2() {
+    count--
+    ttx.textContent = 1 + count;
+    if (count < -1) {
+        ttx.style.backgroundColor = String("#ed239f");
+
+
+    } else if (count < 10) {
+        ttx.style.backgroundColor = "white";
+    }
+    if (count > 10) {
+        ttx.style.color = "white"
+
+    } else if (count < 10) {
+        ttx.style.color = "black"
+    }
+    if (count < -1) {
+        ttx.style.color = "white"
+    }
+
+}
+
 // 2 talik
 
-let in11 = document.querySelector(".in1")
-let in22 = document.querySelector(".in2")
+let in11 = document.querySelector(".in11")
+let in22 = document.querySelector(".in22")
 
 
-function ok11(){
-   
+function ok11() {
+
     count++
-    ttx.textContent = 2 + count ++;
-    if (count >9) {
-        ttx.style.backgroundColor = "red";
-    } 
+    ttx.textContent = 2 + count++;
+    if (count > 8) {
+        ttx.style.backgroundColor = String("#2742DF");
+        ttx.style.color = "white"
 
-    else if(count > -1){
-        ttx.style.backgroundColor = "white"; 
+
+    } else if (count > -2) {
+        ttx.style.backgroundColor = "white";
     }
-    
-   
+
+    if (count > 8) {
+        ttx.style.color = "white"
+
+    } else if (count < 8) {
+        ttx.style.color = "black"
+    }
+    if (count < -1) {
+        ttx.style.color = "white"
+    }
 }
 
-function ok22(){
+function ok22() {
     count--
-    ttx.textContent = 0 + count --;
+    ttx.textContent = 0 + count--;
     if (count < 0) {
-        ttx.style.backgroundColor = "yellow";
-        ttx.style.color.color = "red"
-     
-    }    else if(count < 10){
-        ttx.style.backgroundColor = "white"; 
+        ttx.style.backgroundColor = String("#ed239f");
+
+    } else if (count < 10) {
+        ttx.style.backgroundColor = "white";
     }
+
+    if (count > 10) {
+        ttx.style.color = "white"
+
+    } else if (count < 10) {
+        ttx.style.color = "black"
+    }
+
+    if (count < -2) {
+        ttx.style.color = "white"
+    }
+    if (count < 0) {
+        ttx.style.backgroundColor = "white";
+    }
+    if (count < -1) {
+        ttx.style.backgroundColor = String("#ed239f");
+    }
+
 }
-    
+
+const res = document.getElementById("res");
+res.addEventListener("click" , function(){
+    location.reload();
+});
+
+
 
